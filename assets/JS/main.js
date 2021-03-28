0365733e99d3fd61c5d0b61ca21ed43bac4e165b
DevAnaxes <rahul.mummalaneni@hotmail.com>
2021-03-28 10:09:38 +1000

    Made some changes 🕹.
    Co-Authored-By: Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,42 +1,68 @@
-    document.addEventListener('contextmenu', event => event.preventDefault());
-    jQuery(document).ready(function () {
-        function disableSelection(e) {
-            if (typeof e.onselectstart != "undefined") e.onselectstart = function () {
-                return false
-            };
-            else if (typeof e.style.MozUserSelect != "undefined") e.style.MozUserSelect = "none";
-            else e.onmousedown = function () {
-                return false
-            };
-            e.style.cursor = "default"
-        }
-        window.onload = function () {
-            disableSelection(document.body)
-        };
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+jQuery(document).ready(function () {
+  function disableSelection(e) {
+    if (typeof e.onselectstart != "undefined")
+      e.onselectstart = function () {
+        return false;
+      };
+    else if (typeof e.style.MozUserSelect != "undefined")
+      e.style.MozUserSelect = "none";
+    else
+      e.onmousedown = function () {
+        return false;
+      };
+    e.style.cursor = "default";
+  }
+  window.onload = function () {
+    disableSelection(document.body);
+  };
 
-        window.addEventListener("keydown", function (e) {
-            if (e.ctrlKey && (e.which == 65 || e.which == 66 || e.which == 67 || e.which == 70 || e.which == 73 || e.which == 80 || e.which == 83 || e.which == 85 || e.which == 86)) {
-                e.preventDefault()
-            }
-        });
-        document.keypress = function (e) {
-            if (e.ctrlKey && (e.which == 65 || e.which == 66 || e.which == 70 || e.which == 67 || e.which == 73 || e.which == 80 || e.which == 83 || e.which == 85 || e.which == 86)) { }
-            return false
-        };
+  window.addEventListener("keydown", function (e) {
+    if (
+      e.ctrlKey &&
+      (e.which == 65 ||
+        e.which == 66 ||
+        e.which == 67 ||
+        e.which == 70 ||
+        e.which == 73 ||
+        e.which == 80 ||
+        e.which == 83 ||
+        e.which == 85 ||
+        e.which == 86)
+    ) {
+      e.preventDefault();
+    }
+  });
+  document.keypress = function (e) {
+    if (
+      e.ctrlKey &&
+      (e.which == 65 ||
+        e.which == 66 ||
+        e.which == 70 ||
+        e.which == 67 ||
+        e.which == 73 ||
+        e.which == 80 ||
+        e.which == 83 ||
+        e.which == 85 ||
+        e.which == 86)
+    ) {
+    }
+    return false;
+  };
 
-        document.onkeydown = function (e) {
-            e = e || window.event;
-            if (e.keyCode == 123 || e.keyCode == 18) {
-                return false
-            }
-        };
+  document.onkeydown = function (e) {
+    e = e || window.event;
+    if (e.keyCode == 123 || e.keyCode == 18) {
+      return false;
+    }
+  };
 
-        document.oncontextmenu = function (e) {
-            var t = e || window.event;
-            var n = t.target || t.srcElement;
-            if (n.nodeName != "A") return false
-        };
-        document.ondragstart = function () {
-            return false
-        };
-    });
+  document.oncontextmenu = function (e) {
+    var t = e || window.event;
+    var n = t.target || t.srcElement;
+    if (n.nodeName != "A") return false;
+  };
+  document.ondragstart = function () {
+    return false;
+  };
+});
