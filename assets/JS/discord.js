036a2ad663e3f62eae2c30ce2a4cb6f204cf455a
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2021-08-02 18:02:53 +1000

    🎹 Added word count limits.

--- assets/JS/discord.js
+++ assets/JS/discord.js
@@ -5,7 +5,7 @@ fetch(url).then(function (response) {
       response.json().then(function (data) {
         var discordStatusHTML = document.getElementById("discordStatus_socials");
         var status = `${
-          data.data.active_on_discord_desktop ? data.data.activities[0].emoji.name + ` ${data.data.activities[0].state}` : "Anaxes#3274"
+          data.data.active_on_discord_desktop ? data.data.activities[0].emoji.name + ` ${data.data.activities[0].state.split("", 20).reduce((o, c) => (o.length === 19 ? `${o}${c}...` : `${o}${c}`),"")}` : "Anaxes#3274"
         }`;
         discordStatusHTML.innerHTML += status;
       });
