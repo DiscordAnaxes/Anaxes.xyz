e9453b28829f10b1c7d3b49a7642e3d678d61b80
DiscordAnaxes <anaxesoce@gmail.com>
2021-08-05 16:16:30 +1000

    🧪 Experimenting GitHub API.

--- assets/JS/github.js
+++ assets/JS/github.js
@@ -11,3 +11,68 @@ fetch(url).then(function (response) {
     console.log("Response failed?");
   }
 });
+
+const repos = document.getElementById("repos");
+
+async function getRepo(user) {
+    const result = await fetch(`https://api.github.com/users/DiscordAnaxes/repos`);
+    let repos = await result.json();
+
+    repos = filterRepos(repos);
+    return repos;
+}
+
+function filterRepos(repos) {
+    const foundRepos = [];
+
+    for (repo of repos) {
+        if (repo.fork) continue;
+
+        repo.created_at = new Date(repo.created_at);
+        foundRepos.push(repo);
+    }
+
+    const sortRepo = foundRepos.sort((a, b) => b.created_at - a.created_at);
+
+    return sortRepo.slice(0, 12);
+}
+
+async function addRepos() {
+    for (repo of await getRepo("DiscordAnaxes")) {
+        const repoDiv = document.createElement("div");
+        repoDiv.className = "repo";
+
+        const repoTitle = document.createElement("div");
+        repoTitle.className = "repo-title";
+
+        const repoUrl = document.createElement("a");
+        repoUrl.innerText =
+            repo.name.length > 11
+                ? `${repo.name.substring(0, 8)}...`
+                : repo.name;
+        repoUrl.href = repo.html_url;
+        repoUrl.target = "_blank";
+
+        const tempDiv = document.createElement("div");
+
+        const starCount = document.createElement("p");
+        starCount.innerText = repo.stargazers_count;
+
+        const imgFile = document.createElement("img");
+        imgFile.src = "./static/images/star.svg";
+
+        tempDiv.append(starCount, imgFile);
+        repoTitle.append(repoUrl, tempDiv);
+
+        const repoDescription = document.createElement("p");
+        repoDescription.innerText =
+            repo.description.length > 76
+                ? `${repo.description.substring(0, 73)}...`
+                : repo.description;
+
+        repoDiv.append(repoTitle, repoDescription);
+        repos.appendChild(repoDiv);
+    }
+}
+
+addRepos();
