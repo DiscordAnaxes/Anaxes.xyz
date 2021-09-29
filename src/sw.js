f36559786fe1347c1d471a76639232e1c0300f25
Anaxes <anaxesoce@gmail.com>
2021-09-29 12:15:25 +1000

    🎁 Updated sw.js.

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,13 @@
 "use strict";
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("static").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./assets/css/style.css",
-        "./assets/images/AnaxesLogo192.png",
-      ]);
-    })
-  );
+    e.waitUntil(caches.open("static").then((cache) => {
+        return cache.addAll([
+            "./",
+            "./assets/css/style.css",
+            "./assets/images/AnaxesLogo192.png",
+        ]);
+    }));
+});
+self.addEventListener("fetch", (e) => {
+    console.log(`Intercepting fetch request for: ${e.request.url}`);
 });
