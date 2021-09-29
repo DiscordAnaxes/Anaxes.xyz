96a767387d6f8bb1727b05d8096bb7bec4fb510d
DiscordAnaxes <DiscordAnaxes@users.noreply.github.com>
2021-09-29 02:12:42 +0000

    👀 Prettified code!

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,12 @@
 "use strict";
 self.addEventListener("install", (e) => {
-    e.waitUntil(caches.open("static").then(cache => {
-        return cache.addAll(["./", "./assets/css/style.css", "./assets/images/AnaxesLogo192.png"]);
-    }));
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./assets/css/style.css",
+        "./assets/images/AnaxesLogo192.png",
+      ]);
+    })
+  );
 });
--- src/sw.ts
+++ src/sw.ts
@@ -1,12 +1,15 @@
-self.addEventListener("install", (e:any) => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./assets/css/style.css", "./assets/images/AnaxesLogo192.png"])
-        })
-    );
+self.addEventListener("install", (e: any) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./assets/css/style.css",
+        "./assets/images/AnaxesLogo192.png",
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (e:any) => {
-    console.log(`Intercepting fetch request for: ${e.request.url}`);
-    
+self.addEventListener("fetch", (e: any) => {
+  console.log(`Intercepting fetch request for: ${e.request.url}`);
 });
